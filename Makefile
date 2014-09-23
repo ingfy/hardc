fe190666f12390c1e489a81d9fff31dc7bc68c5b
= <yngve@outlook.com>
2014-09-23 11:30:19 +0200

    Exercise 4: Introducing Valagrind

--- Makefile
+++ Makefile
@@ -4,8 +4,10 @@ clean:
 	rm -f ex1
 	rm -f ex2
 	rm -f ex3
+	rm -f ex4
 
 all:
 	make ex1
 	make ex2
 	make ex3
+	make ex4
---
+++ ex4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Warning: THis program is wrong on purpose */
+int main()
+{
+  int age = 10;
+  int height = 75;
+
+  printf("I am %d years old.\n", age);
+  printf("I am %d inches tall.\n", height);
+
+  return 0;
+}
